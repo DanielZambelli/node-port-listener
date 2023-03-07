f4b01ca234024cb3c2e9c2efd4992313f87d19e3
DQPZ (Daniel Peter Zambelli) <DQPZ@novonordisk.com>
2023-03-07 16:59:41 +0100

    patch

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const fetch = require('node-fetch')
 const app = express()
 const port = 80
 
 app.use(express.text())
 
-app.post('*', (req, res) => {
-  console.log('>> HIT', req.body)
+app.post('*', async (req, res) => {
   res.send('ok'+req.body)
+  await fetch('https://10.1.79.31/api/device/dataString/xxxxxx', {
+    method: 'POST',
+    body: 'xxxxxx'
+  })
+  console.log('>> forwarded', req.body)
 })
 
 app.get('*', (req, res) => {
