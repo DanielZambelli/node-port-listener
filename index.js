b9b290c3d340958bd2527c4742f444d051e7fa41
DQPZ (Daniel Peter Zambelli) <DQPZ@novonordisk.com>
2023-03-07 17:09:11 +0100

    patch

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const port = 80
 
 app.use(express.text())
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
 app.post('*', async (req, res) => {
   res.send('ok'+req.body)
+  console.log('>> hit', req.body)
   await fetch('https://10.1.79.31/api/device/dataString/xxxxxx', {
     method: 'POST',
     body: 'xxxxxx'
   })
-  console.log('>> forwarded', req.body)
 })
 
 app.get('*', (req, res) => {
